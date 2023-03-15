cfa06949b6596aaf1ff2d2bd07417f57f2b8a059
yogesh147 <yogeshch147@gmail.com>
2023-03-15 21:47:59 +0530

    Number Format I18N Demo

--- src/main/java/com/springboot/demo/I18N/I18NDemoApplication.java
+++ src/main/java/com/springboot/demo/I18N/I18NDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -26,6 +27,19 @@ public class I18NDemoApplication {
 
 		log.info("polish local Message :: {}", message);
 		log.info("Local local Message ::  {}", message2);
+
+        System.out.println(":::::::::::::::::::::::::::::: NumberFormat Demo :::::::::::::::::::::::::::::::::::");
+
+        double d = 123456.789;
+        NumberFormat nf = NumberFormat.getInstance(Locale.ITALY);
+        NumberFormat nf1 = NumberFormat.getInstance(Locale.US);
+        NumberFormat nf2 = NumberFormat.getInstance(Locale.CHINA);
+
+        System.out.println("ITALY representation of " + d + " : " + nf.format(d));
+
+        System.out.println("US representation of " + d + " : " + nf1.format(d));
+
+        System.out.println("CHINA representation of " + d + " : " + nf2.format(d));
     }
 
 }
