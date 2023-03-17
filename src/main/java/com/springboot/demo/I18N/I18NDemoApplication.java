e688e5f636584242561be0c94fa9256c48f052d3
yogesh147 <yogeshch147@gmail.com>
2023-03-17 18:38:55 +0530

    currency format I18N Demo

--- src/main/java/com/springboot/demo/I18N/I18NDemoApplication.java
+++ src/main/java/com/springboot/demo/I18N/I18NDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConf
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -25,7 +26,15 @@ public class I18NDemoApplication {
 		ResourceBundle bundle2 = ResourceBundle.getBundle("messages", locale);
 		String message2 = bundle2.getString("label");
 
-		log.info("polish local Message :: {}", message);
+        System.out.println(locale);
+
+        final Locale hinIndiaLocal = new Locale("hi", "IN");
+        System.out.println(Currency.getInstance(hinIndiaLocal).getSymbol());
+
+        final Locale engIndiaLocal = new Locale("EN", "IN");
+        System.out.println(Currency.getInstance(engIndiaLocal).getSymbol());
+
+        log.info("polish local Message :: {}", message);
 		log.info("Local local Message ::  {}", message2);
 
         System.out.println(":::::::::::::::::::::::::::::: NumberFormat Demo :::::::::::::::::::::::::::::::::::");
@@ -34,12 +43,36 @@ public class I18NDemoApplication {
         NumberFormat nf = NumberFormat.getInstance(Locale.ITALY);
         NumberFormat nf1 = NumberFormat.getInstance(Locale.US);
         NumberFormat nf2 = NumberFormat.getInstance(Locale.CHINA);
+        NumberFormat nf3 = NumberFormat.getInstance(engIndiaLocal);
 
         System.out.println("ITALY representation of " + d + " : " + nf.format(d));
-
         System.out.println("US representation of " + d + " : " + nf1.format(d));
-
         System.out.println("CHINA representation of " + d + " : " + nf2.format(d));
+        System.out.println("INDIA representation of " + d + " : " + nf3.format(d));
+
+
+        System.out.println(":::::::::::::::::::::::::::::: currency format Demo :::::::::::::::::::::::::::::::::::");
+
+        Locale localee = Locale.UK;
+        Currency curr = Currency.getInstance(localee);
+        System.out.println("UK Currency symbol is: " + curr.getSymbol(localee));
+
+        Currency currency = Currency.getInstance(locale);
+        System.out.println("Local Currency symbol is: " + currency.getSymbol(locale));
+
+        printCurrency(Locale.UK);
+        printCurrency(Locale.US);
+        printCurrency(Locale.FRANCE);
+        printCurrency(hinIndiaLocal);
+        printCurrency(engIndiaLocal);
+
+    }
+
+    static void printCurrency(Locale locale) {
+        double dbl = 10500.3245;
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(locale);
+        String currency = formatter.format(dbl);
+        System.out.println( currency + " for the locale " + locale);
     }
 
 }
