75c6c5f4a328341f86ff1a4598fe5849463ef1b8
yogesh147 <yogeshch147@gmail.com>
2023-03-18 20:38:56 +0530

    Spel Expression Demo

---
+++ src/main/java/com/springboot/demo/SpelExpression/Inventor.java
@@ -0,0 +1,20 @@
+package com.springboot.demo.SpelExpression;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+public class Inventor {
+
+    public String name;
+
+    public Date birthday;
+
+    public String nationality;
+
+    public String getNameAndNationality() {
+        return name + "" + nationality;
+    }
+
+}
---
+++ src/main/java/com/springboot/demo/SpelExpression/SpelExpressionDemo.java
@@ -0,0 +1,47 @@
+package com.springboot.demo.SpelExpression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.MethodResolver;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+@SpringBootApplication(exclude = SqlInitializationAutoConfiguration.class)
+public class SpelExpressionDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpelExpressionDemo.class, args);
+        ExpressionParser parser = new SpelExpressionParser();
+
+        GregorianCalendar c = new GregorianCalendar();
+        c.set(1856, Calendar.AUGUST, 9);
+
+        var tesla = new Inventor("Nikola Tesla", c.getTime(), "Serbian");
+        System.out.println("class name ::" + tesla.getClass().getName());
+
+        Expression exp = parser.parseExpression("name");
+
+        EvaluationContext context = new StandardEvaluationContext(tesla);
+        String nameFromContext = (String) exp.getValue(context);
+//                          or
+        String nameFromObj = (String) exp.getValue(tesla);
+
+        System.out.println("nameFromContext :: " + nameFromContext);
+        System.out.println("nameFromObj :: " + nameFromObj);
+
+        Expression bolExp = parser.parseExpression("name == 'Nikola Tesla'");
+        boolean result = Boolean.TRUE.equals(bolExp.getValue(context, Boolean.class));
+        System.out.println("boolean result :: " + result);
+
+        final MethodResolver methodResolver = context.getMethodResolvers().get(0);
+        System.out.println("methodResolver" + methodResolver);
+  }
+
+}
