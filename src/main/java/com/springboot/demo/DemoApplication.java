f990b0f6783cde732faad63791432e5fe585ba50
yogesh147 <yogeshch147@gmail.com>
2023-03-31 21:46:24 +0530

    add multiple yml files

--- src/main/java/com/springboot/demo/DemoApplication.java
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println(":::::::::::::::::::::::::::::: Demo App Started on http://localhost:8081/ :::::::::::::::::::::::::::::::::::");
+		System.out.println(":::::::::::::::::::::::::::::: Demo App Started on http://localhost:8080/ :::::::::::::::::::::::::::::::::::");
 	}
 
 }
