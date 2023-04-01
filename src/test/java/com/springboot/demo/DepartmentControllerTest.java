90c4bcd940e70279daa8335478bd5257ab6ce445
yogesh147 <yogeshch147@gmail.com>
2023-04-01 14:51:47 +0530

    Test factory demo

--- src/test/java/com/springboot/demo/DepartmentControllerTest.java
+++ src/test/java/com/springboot/demo/DepartmentControllerTest.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.function.ThrowingConsumer;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -123,12 +124,52 @@ class DepartmentControllerTest {
         Assertions.assertTrue(Numbers.isOdd(number));
     }
 
+    @TestFactory
+    Iterable<DynamicTest> dynamicTestsWithCollection() {
+        return Arrays.asList(
+                DynamicTest.dynamicTest("Add test",
+                        () -> Assertions.assertEquals(2, Math.addExact(1, 1))),
+                DynamicTest.dynamicTest("Multiply Test",
+                        () -> Assertions.assertEquals(4, Math.multiplyExact(2, 2))));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsFromStream() {
+
+        // sample input and output
+        List<String> inputList = Arrays.asList("www.somedomain.com", "www.anotherdomain.com", "www.yetanotherdomain.com");
+        List<String> outputList = Arrays.asList("154.174.10.56", "211.152.104.132", "178.144.120.156");
+
+        // input generator that generates inputs using inputList
+        Iterator<String> inputGenerator = inputList.iterator();
+
+        // a display name generator that creates a different name based on the input
+        Function<String, String> displayNameGenerator = (input) -> "Resolving: " + input;
+
+        // the test executor, which actually has the logic to execute the test case
+        DomainNameResolver resolver = new DomainNameResolver();
+        ThrowingConsumer<String> testExecutor = (input) -> {
+            int id = inputList.indexOf(input);
+
+            Assertions.assertEquals(outputList.get(id), resolver.resolveDomain(input));
+        };
+        // combine everything and return a Stream of DynamicTest
+        return DynamicTest.stream( inputGenerator, displayNameGenerator, testExecutor);
+    }
+
 }
 
 class Numbers {
     public static boolean isOdd(int number) {
         return number % 2 != 0;
     }
+}
 
-
+class DomainNameResolver {
+    public String resolveDomain(String input) {
+        if (input.equalsIgnoreCase("www.somedomain.com")) return "154.174.10.56";
+        if (input.equalsIgnoreCase("www.anotherdomain.com")) return "211.152.104.132";
+        if (input.equalsIgnoreCase("www.yetanotherdomain.com")) return "178.144.120.156";
+        return input;
+    }
 }
